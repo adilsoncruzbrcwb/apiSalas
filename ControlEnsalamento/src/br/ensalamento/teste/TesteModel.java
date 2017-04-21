0cd80875e431dc164078d41afd73bd1bb552cecd
Felipe <Felipe@DESKTOP-0CC8LEM>
2017-04-21 18:38:04 -0300

    TesteModel

---
+++ ControlEnsalamento/src/br/ensalamento/teste/TesteModel.java
@@ -0,0 +1,13 @@
+package br.ensalamento.teste;
+
+import br.ensalamento.hibernate.HibernateUtil;
+
+public class TesteModel {
+	
+	public static void main (String []args){
+		HibernateUtil.iniciarTransacao();
+		HibernateUtil.confirmarTransacao();
+		HibernateUtil.fecharConexao();
+	}
+
+}
